47129f6fdf0bbaf35c65e3075d9f1f6d389d7322
Douglas Silva <douglas.edig@outlook.com>
2024-07-09 01:31:33 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Douglas Matheus da Silva on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
