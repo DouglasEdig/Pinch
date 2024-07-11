ae93410266ea41184c8230a7463257440dbe2383
Douglas Silva <douglas.edig@outlook.com>
2024-07-10 23:57:51 -0300

    feat: add image in contentView

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+      }
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
     }
+    .navigationViewStyle(.stack)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
