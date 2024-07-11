de818fa86d9d59af2cf3d3088c026b559090d2f4
Douglas Silva <douglas.edig@outlook.com>
2024-07-11 00:10:39 -0300

    feat: double tap gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+  
+  
+  
   var body: some View {
     NavigationView {
       ZStack {
@@ -17,9 +23,28 @@ struct ContentView: View {
           .cornerRadius(10)
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .scaleEffect(imageScale)
+        //MARK: - 1. TAP GESTURE
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1
+              }
+            }
+          })
       }
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform:  {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
+        }
+      })
     }
     .navigationViewStyle(.stack)
   }
